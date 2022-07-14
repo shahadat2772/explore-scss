8b713c7ffef35d11142df12c90d96bb85ba7a0f7
shahadat2772 <shahadat2772@gmailcom>
2022-07-14 18:30:01 +0600

    completed the banner

--- src/pages/Banner/Banner.js
+++ src/pages/Banner/Banner.js
@@ -1,3 +1,5 @@
+import { faTag } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Banner.scss";
 
@@ -7,6 +9,9 @@ const Banner = () => {
       <div className="bannerContainer">
         <div className="bannerImg">
           <div className="bannerContent">
+            <p>
+              <FontAwesomeIcon className="tagIcon" icon={faTag} /> History
+            </p>
             <h2>
               Heimdall, guard and <br /> keeper of the <br /> Ragnarök
             </h2>
